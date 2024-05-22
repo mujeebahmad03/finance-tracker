f8699141893624bd1fc0eca14f8bc38d87be6c34
Abdulmujeeb Ahmad <abdulmujeebahmad03@gmail.com>
2024-05-22 05:58:50 +0100

    fix logo on auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import type { ReactNode } from "react";
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
-      <Logo />
+      <Logo loading={false} mobile={false} />
       <div className="mt-12">{children}</div>
     </div>
   );
